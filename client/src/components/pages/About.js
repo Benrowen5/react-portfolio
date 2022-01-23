cbcdaebd627d434d94946465115e5293b783a9aa
Benrowen5 <79202800+Benrowen5@users.noreply.github.com>
2022-01-23 11:39:16 -0500

    fixed footer positioning, added about me content.

--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -12,6 +12,7 @@ function About () {
                 <h2>About Me</h2>
                 <p>As a mechanical engineer by degree, after working professionally for 7 years I decided that I wanted (and needed) to make a change in my career. Working as an engineer in the consumer products industry was very enjoyable, but not what I was passionate about. I decided to pursure something I found myself to be more interested in.</p>
                 <p>I made the decision to sign up for Case Western Reserve University's Full Stack Web Developer Coding Bootcamp. Through this program I would learn the in demand skills used by software engineers, web developers, and entrepeneurs alike, and begin my journey to becoming a professional developer.</p>
+                <p>Some of my interests and hobbies include: coding, home improvement projects, weight lifting, running and hiking, ice hockey, dogs, self-improvement, guitar, and continuous learning.</p>
             </div>
         </section>
     );   
