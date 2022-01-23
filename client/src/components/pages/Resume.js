32bd7563f1286e4f469583ee94f9e76aca23edc7
Benrowen5 <79202800+Benrowen5@users.noreply.github.com>
2022-01-23 08:57:05 -0500

    added resume view/download link to resume component

--- client/src/components/pages/Resume.js
+++ client/src/components/pages/Resume.js
@@ -3,9 +3,13 @@ import React from 'react';
 function Resume () {
     
     return (
-        <div>
-            <h1>Resume</h1>
-        </div>
+        <section className="resume-section">
+            <h3>Click below to view/download my resume.</h3>
+                <a href= 'https://drive.google.com/file/d/1wXxQBcxe6YZQZZ94x48GT4Zf4ndaXPOD/view?usp=sharing' target="_blank">
+                    <img src="https://img.icons8.com/office/80/000000/pdf.png" alt='pdf resume download'/>                
+                </a>
+                <p>Benjamin Rowen Resume</p>
+        </section>
     );   
 }
 
