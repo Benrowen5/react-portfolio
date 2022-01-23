4d73dbc585d3569d656987522b095d6e845b2d56
Benrowen5 <79202800+Benrowen5@users.noreply.github.com>
2022-01-23 09:28:14 -0500

    added icons for proficiencies to resume component

--- client/src/components/pages/Resume.js
+++ client/src/components/pages/Resume.js
@@ -4,11 +4,24 @@ function Resume () {
     
     return (
         <section className="resume-section">
-            <h3>Click below to view/download my resume.</h3>
-                <a href= 'https://drive.google.com/file/d/1wXxQBcxe6YZQZZ94x48GT4Zf4ndaXPOD/view?usp=sharing' target="_blank">
-                    <img src="https://img.icons8.com/office/80/000000/pdf.png" alt='pdf resume download'/>                
-                </a>
-                <p>Benjamin Rowen Resume</p>
+            <div className='skills'>
+                <h3>Proficiencies:</h3>
+                <img src="https://img.icons8.com/color/50/000000/javascript--v1.png" alt="Javascript icon"/>
+                <img src="https://img.icons8.com/color/48/000000/html-5--v1.png" alt="HTML5 icon"/>
+                <img src="https://img.icons8.com/color/48/000000/nodejs.png" alt="NodeJS icon"/>
+                <img src="https://img.icons8.com/color/48/000000/mongodb.png" alt="MongoDB icon"/>
+                <img src="https://img.icons8.com/color/48/000000/heroku.png" alt="Heroku icon"/>
+                <img src="https://img.icons8.com/ios-filled/50/000000/git.png" alt="Git source control icon"/>
+                <img src="https://img.icons8.com/color/48/000000/react-native.png" alt="React icon"/>
+                <img src="https://img.icons8.com/color/48/000000/css3.png" alt="CSS3 icon"/>
+            </div>
+            <div>
+                <h4>Click below to view/download my resume.</h4>
+                    <a href= 'https://drive.google.com/file/d/1wXxQBcxe6YZQZZ94x48GT4Zf4ndaXPOD/view?usp=sharing' target="_blank">
+                        <img src="https://img.icons8.com/office/80/000000/pdf.png" alt='pdf resume download'/>                
+                    </a>
+                    <p>Benjamin Rowen Resume</p>
+            </div>
         </section>
     );   
 }
