84c681633f86072d508875ee5ba6b4b40617f4f4
Benrowen5 <79202800+Benrowen5@users.noreply.github.com>
2022-01-23 18:00:31 -0500

    Update Resume.js
    linked updated resume.

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -17,7 +17,7 @@ function Resume () {
             </div>
             <div>
                 <h4>Click below to view/download my resume.</h4>
-                    <a href= 'https://drive.google.com/file/d/1wXxQBcxe6YZQZZ94x48GT4Zf4ndaXPOD/view?usp=sharing' target="_blank">
+                    <a href= 'https://drive.google.com/file/d/1fXuBORjGgKz4BAp_jKKGBSqsSb4yswXs/view?usp=sharing' target="_blank">
                         <img src="https://img.icons8.com/office/80/000000/pdf.png" alt='pdf resume download'/>                
                     </a>
                     <p>Benjamin Rowen Resume</p>
